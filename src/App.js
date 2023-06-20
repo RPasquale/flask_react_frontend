f055b7dbdb658b6f9d263d0655c49a6d9f60921c
Robbie Pasquale <rpas0004@student.monash.edu>
2023-06-20 18:32:18 +1000

    working initial

--- src/App.js
+++ src/App.js
@@ -8,18 +8,8 @@ function App() {
     event.preventDefault();
 
     try {
-      const responseDiabetes = await axios.post(
-        "http://localhost:5000/predict/diabetes",
-        {
-          feature1: event.target.feature1.value,
-          feature2: event.target.feature2.value,
-          // Add more features as needed
-        }
-      );
-      const { prediction: diabetesPrediction } = responseDiabetes.data;
-
       const responseHousing = await axios.post(
-        "http://localhost:5000/predict/housing",
+        "http://localhost:5000/predict",
         {
           feature1: event.target.feature1.value,
           feature2: event.target.feature2.value,
@@ -29,15 +19,13 @@ function App() {
           feature6: event.target.feature6.value,
           feature7: event.target.feature7.value,
           feature8: event.target.feature8.value,
-          feature9: event.target.feature9.value,
-          feature10: event.target.feature10.value,
+
           // Add more features as needed
         }
       );
       const { prediction: housingPrediction } = responseHousing.data;
 
       const predictions = {
-        diabetes: diabetesPrediction,
         housing: housingPrediction,
       };
 
@@ -52,56 +40,44 @@ function App() {
       <h1>House Price Prediction</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="feature1">Longitude:</label>
-          <input type="text" id="feature1" name="feature1" />
+          <label htmlFor="feature1">
+            Median Income of Households in a block:
+          </label>
+          <input type="number" id="feature1" name="feature1" />
         </div>
         <div>
-          <label htmlFor="feature2">Latitude:</label>
-          <input type="text" id="feature2" name="feature2" />
-        </div>
-        <div>
-          <label htmlFor="feature3">
+          <label htmlFor="feature2">
             Median Age of a House within a block:
           </label>
-          <input type="text" id="feature3" name="feature3" />
+          <input type="number" id="feature2" name="feature2" />
+        </div>
+        <div>
+          <label htmlFor="feature3">Average Number of Rooms in a Block:</label>
+          <input type="number" id="feature3" name="feature3" />
         </div>
         <div>
           <label htmlFor="feature4">
-            Total Number of Rooms within a Block:
+            Average Number of Bedrooms in a Block
           </label>
-          <input type="text" id="feature4" name="feature4" />
+          <input type="number" id="feature4" name="feature4" />
         </div>
         <div>
           <label htmlFor="feature5">
-            Total Number of Bedrooms within a Block:
-          </label>
-          <input type="text" id="feature5" name="feature5" />
-        </div>
-        <div>
-          <label htmlFor="feature6">
             Total Number of people residing within a block:
           </label>
-          <input type="text" id="feature6" name="feature6" />
+          <input type="number" id="feature5" name="feature5" />
         </div>
         <div>
-          <label htmlFor="feature7">Total Number of Households:</label>
-          <input type="text" id="feature7" name="feature7" />
+          <label htmlFor="feature6">Average Occupancy:</label>
+          <input type="number" id="feature6" name="feature6" />
         </div>
         <div>
-          <label htmlFor="feature8">
-            Median Income of Households in a block:
-          </label>
-          <input type="text" id="feature8" name="feature8" />
+          <label htmlFor="feature7">Latitude:</label>
+          <input type="number" id="feature7" name="feature7" />
         </div>
         <div>
-          <label htmlFor="feature9">
-            Median House value of Households in a block:
-          </label>
-          <input type="text" id="feature9" name="feature9" />
-        </div>
-        <div>
-          <label htmlFor="feature10">Ocean Proximity:</label>
-          <input type="text" id="feature10" name="feature10" />
+          <label htmlFor="feature8">Longitude: </label>
+          <input type="number" id="feature8" name="feature8" />
         </div>
 
         {/* Add more input fields for other features */}
@@ -110,8 +86,6 @@ function App() {
       <div>
         <h2>Prediction Results</h2>
         <p>Housing Prediction: {predictionResult.housing}</p>
-
-        <p>Diabetes Prediction: {predictionResult.diabetes}</p>
       </div>
     </div>
   );
